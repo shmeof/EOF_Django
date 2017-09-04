fba34ed38575679042b7895a082fa8d6fa450810
eof <eof000@qq.com>
2017-09-04 13:48:31 +0800

    config

---
+++ test_uwsgi.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+import os
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")    #project为项目名，请按需求修改
+application = get_wsgi_application()
